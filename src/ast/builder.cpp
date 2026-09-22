59a76744db9b3b240454c56d84fad2697531d3e1
TfOi9 <tfoi92024@sjtu.edu.cn>
2026-09-22 14:52:08 +0800

    feat: added builder for function item

--- src/ast/builder.cpp
+++ src/ast/builder.cpp
@@ -1,5 +1,6 @@
 #include "builder.hpp"
 #include "ast.hpp"
+#include <optional>
 
 namespace ast {
 
@@ -19,15 +20,49 @@ SourceSpan AstBuilder::makeSpan(antlr4::ParserRuleContext* ctx) const {
     auto* endToken = ctx->stop;
     return SourceSpan {
         SourceLocation {
-            beginToken->getTokenIndex(),
+            beginToken->getStartIndex(),
             beginToken->getLine(),
             beginToken->getCharPositionInLine()
         },
         SourceLocation {
-            endToken->getTokenIndex() + 1,
+            endToken->getStopIndex() + 1,
             endToken->getLine(),
             endToken->getCharPositionInLine() + endToken->getText().size()
         }
+    };
+}
+
+std::string AstBuilder::getText(antlr4::tree::TerminalNode* node) const {
+    return node->getText();
+}
+
+std::string AstBuilder::buildIdentifier(RxParser::IdentifierContext* ctx) {
+    return ctx->toString();
+}
+
+Lifetime AstBuilder::buildLifetime(RxParser::LifetimeContext* ctx) {
+    return Lifetime {
+        makeSpan(ctx),
+        ctx->LIFETIME()->getText()
+    };
+}
+
+LifetimeBounds AstBuilder::buildLifetimeBounds(RxParser::LifetimeBoundsContext* ctx) {
+    std::vector<Lifetime> lifetimes;
+    for (auto* lt: ctx->lifetime()) {
+        lifetimes.push_back(buildLifetime(lt));
+    }
+    return LifetimeBounds {
+        makeSpan(ctx),
+        lifetimes
+    };
+}
+
+IdentifierBinding AstBuilder::buildIdentifierBinding(RxParser::IdentifierBindingContext* ctx) {
+    return IdentifierBinding {
+        makeSpan(ctx),
+        ctx->MUT() == nullptr,
+        ctx->identifier()->getText()
     };
 }
 
@@ -49,6 +84,92 @@ AstPtr<Item> AstBuilder::buildItem(RxParser::ItemContext* ctx) {
         return nullptr;
     }
     return nullptr;
+}
+
+std::vector<GenericParam> AstBuilder::buildGenericParams(RxParser::GenericParamsContext* ctx) {
+    std::vector<GenericParam> params;
+    for (auto* param: ctx->lifetimeParam()) {
+        params.push_back(buildGenericParam(param));
+    }
+    return params;
+}
+
+GenericParam AstBuilder::buildGenericParam(RxParser::LifetimeParamContext* ctx) {
+    std::optional<LifetimeBounds> bounds = std::nullopt;
+    if (ctx->lifetimeBounds()) {
+        bounds = buildLifetimeBounds(ctx->lifetimeBounds());
+    }
+    return GenericParam {
+        makeSpan(ctx),
+        buildLifetime(ctx->lifetime()),
+        bounds
+    };
+}
+
+SelfParam AstBuilder::buildSelfParam(RxParser::SelfParamContext* ctx) {
+    return SelfParam {
+        makeSpan(ctx),
+        ctx->AMP() != nullptr,
+        ctx->MUT() != nullptr,
+        ctx->lifetime() ? std::optional<Lifetime>(buildLifetime(ctx->lifetime())) : std::nullopt
+    };
+}
+
+std::vector<FunctionParam> AstBuilder::buildFunctionParams(RxParser::FunctionParametersContext* ctx) {
+    std::vector<FunctionParam> params;
+    for (auto* param: ctx->functionParam()) {
+        params.push_back(buildFunctionParam(param));
+    }
+    return params;
+}
+
+FunctionParam AstBuilder::buildFunctionParam(RxParser::FunctionParamContext* ctx) {
+    return FunctionParam {
+        makeSpan(ctx),
+        buildIdentifier(ctx->identifierBinding()->identifier()),
+        ctx->identifierBinding()->MUT() != nullptr,
+        buildTypeRef(ctx->typeRef())
+    };
+}
+
+WhereClause AstBuilder::buildWhereClause(RxParser::WhereClauseContext* ctx) {
+    WhereClause clause;
+    clause.span = makeSpan(ctx);
+    for (auto* item: ctx->whereClauseItem()) {
+        clause.items.push_back(buildWhereClauseItem(item));
+    }
+    return clause;
+}
+
+WhereClauseItem AstBuilder::buildWhereClauseItem(RxParser::WhereClauseItemContext* ctx) {
+    WhereClauseItem item;
+    item.span = makeSpan(ctx);
+    if (ctx->lifetime() != nullptr) {
+        item.lifetime = LifetimeWhereClauseItem {
+            makeSpan(ctx),
+            buildLifetime(ctx->lifetime()),
+            buildLifetimeBounds(ctx->lifetimeBounds())
+        };
+    } else if (ctx->typeRef() != nullptr) {
+        item.type_bound = TypeBoundWhereClauseItem {
+            makeSpan(ctx),
+            buildTypeRef(ctx->typeRef()),
+            buildTypeParamBounds(ctx->typeParamBounds())
+        };
+    }
+    return item;
+}
+
+AstPtr<FunctionItem> AstBuilder::buildFunctionItem(RxParser::FunctionDefinitionContext* ctx) {
+    FunctionItem fn(makeSpan(ctx));
+    fn.name = buildIdentifier(ctx->identifier());
+    fn.body = buildBlockExpression(ctx->blockExpression());
+    fn.generic_params = ctx->genericParams() ? buildGenericParams(ctx->genericParams()) : std::vector<GenericParam>();
+    fn.self_param = ctx->functionParameters() ? std::optional<SelfParam>(buildSelfParam(ctx->functionParameters()->selfParam())) : std::nullopt;
+    fn.function_params = ctx->functionParameters() ? buildFunctionParams(ctx->functionParameters()) : std::vector<FunctionParam>();
+    fn.where_clause = ctx->whereClause() ? std::optional<WhereClause>(buildWhereClause(ctx->whereClause())) : std::nullopt;
+    fn.return_type = ctx->typeRef() ? buildTypeRef(ctx->typeRef()) : nullptr;
+    return std::make_unique<FunctionItem>(std::move(fn));
 }
 
 } // namespace ast
--- src/ast/builder.hpp
+++ src/ast/builder.hpp
@@ -16,12 +16,25 @@ private:
     std::string_view source_;
 
     SourceSpan makeSpan(antlr4::ParserRuleContext* ctx) const;
+    std::string getText(antlr4::tree::TerminalNode* node) const;
+    std::string buildIdentifier(RxParser::IdentifierContext* ctx);
+    Lifetime buildLifetime(RxParser::LifetimeContext* ctx);
+    LifetimeBounds buildLifetimeBounds(RxParser::LifetimeBoundsContext* ctx);
+    IdentifierBinding buildIdentifierBinding(RxParser::IdentifierBindingContext* ctx);
     
     AstPtr<Item> buildItem(RxParser::ItemContext* ctx);
     AstPtr<FunctionItem> buildFunctionItem(RxParser::FunctionDefinitionContext* ctx);
     AstPtr<StructItem> buildStructItem(RxParser::StructDefinitionContext* ctx);
     AstPtr<ConstantItem> buildConstantItem(RxParser::ConstantItemContext* ctx);
     AstPtr<ImplItem> buildImplItem(RxParser::InherentImplContext* ctx);
+
+    std::vector<GenericParam> buildGenericParams(RxParser::GenericParamsContext* ctx);
+    GenericParam buildGenericParam(RxParser::LifetimeParamContext* ctx);
+    SelfParam buildSelfParam(RxParser::SelfParamContext* ctx);
+    std::vector<FunctionParam> buildFunctionParams(RxParser::FunctionParametersContext* ctx);
+    FunctionParam buildFunctionParam(RxParser::FunctionParamContext* ctx);
+    WhereClause buildWhereClause(RxParser::WhereClauseContext* ctx);
+    WhereClauseItem buildWhereClauseItem(RxParser::WhereClauseItemContext* ctx);
 
     AstPtr<Statement> buildStatement(RxParser::StatementContext* ctx);
     AstPtr<ExpressionStatement> buildExpressionStatement(RxParser::StatementExpressionContext* ctx);
@@ -31,6 +44,7 @@ private:
     AstPtr<Expression> buildConditionExpression(RxParser::ConditionExpressionContext* ctx);
 
     AstPtr<TypeRef> buildTypeRef(RxParser::TypeRefContext* ctx);
+    TypeParamBounds buildTypeParamBounds(RxParser::TypeParamBoundsContext* ctx);
     AstPtr<ConstValue> buildConstValue(RxParser::ConstValueContext* ctx);
     
     PathInExpression buildPathInExpression(RxParser::PathInExpressionContext* ctx);
